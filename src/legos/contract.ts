a5c268ccf872e7bc7a6f149dab99a474942070b7
skuhlmann <samkuhlmann@gmail.com>
2023-11-15 15:31:12 -0700

    starts yeet tx

--- src/legos/contract.ts
+++ src/legos/contract.ts
@@ -1,6 +1,7 @@
 import { ContractLego } from "@daohaus/utils";
 
 import yeeterSummonerAbi from "../abis/yeeterSummoner.json";
+import yeeterShamanAbi from "../abis/yeeterShaman.json";
 import { YEETER_CONTRACTS } from "../utils/constants";
 
 export const APP_CONTRACT: Record<string, ContractLego> = {
@@ -9,5 +10,11 @@ export const APP_CONTRACT: Record<string, ContractLego> = {
     contractName: "YeeterSummoner",
     abi: yeeterSummonerAbi,
     targetAddress: YEETER_CONTRACTS["ONBOARDER_SUMMONER"],
+  },
+  YEETER_SHAMAN: {
+    type: "static",
+    contractName: "YeeterShaman",
+    abi: yeeterShamanAbi,
+    targetAddress: ".shamanAddress",
   },
 };
